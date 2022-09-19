28f5b93f0a3dfa3b5d2ec5ca06b66c2eb06c57bf
liankui <yuekewin@gmail.com>
2022-09-20 07:56:03 +0800

    add http server

---
+++ handlers/hellohandler.go
@@ -0,0 +1,19 @@
+package handlers
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+)
+
+type Hello struct {
+	l *log.Logger
+}
+
+func NewHello(l *log.Logger) *Hello {
+	return &Hello{l}
+}
+
+func (h *Hello) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	fmt.Fprint(w, "hello world\n")
+}
